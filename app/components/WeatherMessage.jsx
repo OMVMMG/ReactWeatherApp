d109cc98fed0c41274b4b0a922d4859641420adf
MMG <ommv@yahoo.com>
2017-03-29 00:06:03 +1100

    Add custom styles support

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -13,7 +13,7 @@ var React = require('react');
 var WeatherMessage = ({temp, location}) => {
     //  var {temp, location} = props;
     return (
-        <h3 className="text-center">It is {temp} in {location}</h3>
+        <h3 className="text-center">It is {temp} &deg;C in {location}</h3>
     );
 };
 
